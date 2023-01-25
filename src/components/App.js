e3b2165bcba533229e7982611146ce24d202a685
Ujjwalpandit <99593805+Ujjwalpandit@users.noreply.github.com>
2023-01-25 22:35:28 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,24 @@
-import React from 'react'
-import '../styles/App.css';
+import React  from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "../styles/App.css";
+import Login from "./Login/Login";
+import Master from "./Flight/Master";
+import MasterHotel from "./Hotel/MasterHotel";
+import Navbar from "./Navbar/Navbar";
+import Checkout from "./CheckOut/Checkout";
+
 const App = () => {
-
-
   return (
-    <div id="main">
-    </div>
-  )
-}
-
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route exact path='/' element={<MasterHotel />} />
+        <Route exact path='/masterFlight' element={<Master />} />
+        <Route exact path='/login' element={<Login />} />
+        <Route exact path='/checkout' element={<Checkout />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
